204cecf3bc8531572b3e5df65683f7f025184f98
Mrcomputer1 <Mrcomputer1@users.noreply.github.com>
2015-06-07 21:51:39 +1000

    Small fix for version 1.0.5

--- MinecraftExtension.js
+++ MinecraftExtension.js
@@ -289,7 +289,7 @@
 			['R', 'Online players of %s %n', 'getOnlinePlayers', '', 25565],
 			['R', 'Max players of %s %n', 'getMaxPlayers', '', 25565],
 			['R', 'Server Software of %s %n', 'getServerSoftware', '', 25565],
-			['R', 'Is %s %n %m.mcVersion', 'isVersion', '', 25565, '1.8.5-1.8'],
+			['R', 'Is %s %n %m.mcVersion', 'isVersion', '', 25565, '1.8.7-1.8'],
 			['R', 'Get Version %s %n', 'getVersion', '', 25565],
 			['R', 'Get Mojang Status %m.mojangStatus', 'getMojangStatus', 'minecraft.net'],
 			['r', 'Server IP %m.server', 'getServerIP', ''],
